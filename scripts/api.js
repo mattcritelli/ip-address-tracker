cc1c3a1ff4463f2f008518d84d3dd2a393285600
Matt C <matthew.critelli@gmail.com>
2020-09-07 10:23:23 -0600

    Add ability to update map based on user query.

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,7 @@
-import { updateMap } from './map.js';
+import { loadMap, updateMap } from './map.js';
 
 // Default IP
-let ipAddress = '8.8.8.8';
+// let ipAddress = '8.8.8.8';
 
 // DOM SELECTORS
 const ipEl = document.querySelector('#ip-address');
@@ -14,38 +14,51 @@ const loader = document.querySelector('#loader');
 searchBtn.addEventListener('click', function (e) {
   e.preventDefault();
   const seachText = document.querySelector('#site-search').value;
-  onLoad(seachText);
+  findLocation(seachText);
 })
 
+// FETCH USER IP ON PAGE LOAD
 async function fetchUserIP() {
   const ipApi = 'https://api.ipify.org/?format=json'
   const response = await fetch(ipApi);
   const ipData = await response.json();
-  ipAddress = ipData.ip;
-  return ipAddress;
+  return ipData.ip;
 }
 
-async function fetchIPLocData(ip, callback) {
+// FETCH LOCATION DATA BASED ON USER SEARCH
+async function fetchIPLocData(ip, callback, initialLoad) {
   const apiUrl = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&domain=${ip}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
-  callback(data)
+  callback(data, initialLoad)
 }
 
-function updateDisplayWithResponse({ location, isp, ip }) {
+// UPDATE UI WITH API RESPONSE
+function updateDisplayWithResponse({ location, isp, ip }, initialLoad) {
   ipEl.innerText = ip;
   locationEl.innerText = `${location.city}, ${location.region} ${location.postalCode} `;
   timezoneEl.innerText = `${location.timezone} UTC`;
   ispEl.innerText = isp;
-  updateMap(location.lat, location.lng)
+
+  if (initialLoad) {
+    loadMap(location.lat, location.lng)
+  } else {
+    updateMap(location.lat, location.lng)
+  }
   loader.style.display = "none";
 }
 
 
-async function onLoad(query = null) {
+async function findLocation(query = null) {
   loader.style.display = "block";
-  const ip = query ? query : await fetchUserIP();
-  fetchIPLocData(ip, updateDisplayWithResponse);
+  const initialLoad = query ? false : true;
+  const ipAddress = query ? query : await fetchUserIP();
+
+  if (query) {
+    fetchIPLocData(ipAddress, updateDisplayWithResponse, initialLoad);
+  } else {
+    fetchIPLocData(ipAddress, updateDisplayWithResponse, initialLoad);
+  }
 }
 
-onLoad();
+findLocation();
--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,7 @@
-
-
 var mymap = null;
-// lat: 34.0522
-// lng: -118.244
 
-function updateMap(lat, lng) {
+// Create and load initial map component on page load
+function loadMap(lat, lng) {
   mymap = L.map('mapid').setView([lat, lng], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -15,4 +12,9 @@ function updateMap(lat, lng) {
       accessToken: mapToken
   }).addTo(mymap);
 }
-export { updateMap }
+
+function updateMap(lat, lng) {
+  mymap.setView([lat, lng], 13);
+}
+
+export { loadMap, updateMap }
